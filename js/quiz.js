8abb786ca54a41764a2b1a14654dd094cfc46254
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-13 14:12:39 -0400

    Add share buttons with custom function (WIP)

--- js/quiz.js
+++ js/quiz.js
@@ -31,6 +31,7 @@
 				}
 				$scope.myQuestions[qIndex].questionState = 'answered';
 			}
+			$scope.percentage = (($scope.score / $scope.totalQuestions) * 100).toFixed(0);
 
 		}
 
@@ -44,6 +45,19 @@
 
 		$scope.selectContinue = function(){
 			return $scope.activeQuestion += 1;
+		}
+
+		$scope.createShareLinks = function(percentage){
+			var url = 'http://codifydesign.com';
+			
+			var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored a '+ percentage +'% on this quiz about Saturn. Try to beat my score at '+url+'">Email a Friend</a>';
+			
+			var twitterLink = '<a class="btn twitter" target="_blank" href="https://twitter.com/intent/tweet?text=Check%20out%20this%20game!">Tweet Your Score</a>'
+			// var twitterLink = '<a class="btn twitter" target="_blank" href="https://twitter.com/intent/tweet?text=I scored a '+ percentage +'% on this quiz about Saturn. Try to beat my score at&amp;hashtags=SaturnQuiz&amp;url='+ url +'">Tweet Your Score</a>';
+			
+			var newMarkup = emailLink + twitterLink;
+
+			return $sce.trustAsHtml(newMarkup);
 		}
 
 	}]);
