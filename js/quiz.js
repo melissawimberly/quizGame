20f5fc9cba023572c7a969c3594d7130465edd3a
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-12 23:47:36 -0400

    Fix typo

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,18 @@
 (function(){
 
-	var app = agular.module({'myQuiz', []);
+	var app = angular.module('myQuiz', []);
 
-	app.controller('QuizController', ['$scope', '$http', '$sce', funtion($scope, $http, $sce){
-		
+	app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
+
+		$http.get('quiz_data.json').then(function(quizData){
+			$scope.myQuestions = quizData.data;
+			$scope.totalQuestions = $scope.myQuestions.length;
+
+		});
+
 	}]);
 })();
